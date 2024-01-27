c1e8b9338aa788c02b1b090223a75bd8223d9c93
Rafael Lawisch <lawisch.nh@gmail.com>
2024-01-27 17:05:11 -0300

    chore: fix typo

--- src/lib/components/MatchSelection.svelte
+++ src/lib/components/MatchSelection.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	export let tournamentLevel: string = "Qualification";
+	export let tournamentLevel: string = 'Qualification';
 	export let matchNumber: number = 1;
 
 	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
 
-	import NumberInput from "$lib/components/NumberInput.svelte";
+	import NumberInput from '$lib/components/NumberInput.svelte';
 </script>
 
 <div class="inline-flex justify-center w-full gap-2">
 	<RadioGroup>
-		<RadioItem bind:group={tournamentLevel} name="justify" value={"Qualification"}>Qualy</RadioItem>
-		<RadioItem bind:group={tournamentLevel} name="justify" value={"Playoff"}>Play-off</RadioItem>
+		<RadioItem bind:group={tournamentLevel} name="justify" value={'Qualification'}>Quali</RadioItem>
+		<RadioItem bind:group={tournamentLevel} name="justify" value={'Playoff'}>Play-off</RadioItem>
 	</RadioGroup>
 
 	<NumberInput bind:value={matchNumber} min={1} max={150} />
