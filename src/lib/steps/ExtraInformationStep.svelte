07ac943487d15b5c5fa54086e63b33f3892b5c58
ljb6 <luccajbecker@gmail.com>
2024-02-22 17:08:11 -0300

    some fixes

--- src/lib/steps/ExtraInformationStep.svelte
+++ src/lib/steps/ExtraInformationStep.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import RadioGroup from '$lib/components/RadioGroup.svelte';
-
+	import {foulsStore} from '$lib/store.ts';
+	
 	export let playedAsDefense = false;
 	export let driverSkills = 0;
 	export let robotFailed = false;
@@ -16,7 +16,8 @@
 		{value: "5", label: "5"}
 	]
 
-
+	import RadioGroup from '$lib/components/RadioGroup.svelte';
+	import NumberInput from '$lib/components/NumberInput.svelte';
 	import Rating from '$lib/components/Rating.svelte';
 	import Switch from '$lib/components/Switch.svelte';
 	import TextInputField from '$lib/components/TextInputField.svelte';
@@ -29,7 +30,11 @@
 
 <Step>
 	<svelte:fragment slot="header">Extra information</svelte:fragment>
-	<section class="justify-items-left grid gap-3">
+	<div class="flex justify-items-center alignitems-center gap-3">
+		<h3 class="h3 pb-2 pt-2">Fouls:</h3>
+		<NumberInput bind:value={$foulsStore} min={0} max={5} />
+	</div>
+	<section class="justify-items-left grid gap-3 ">
 		<Switch bind:checked={robotFailed} label="Robot failed?" />
 		<Switch
 			bind:checked={playedAsDefense}
