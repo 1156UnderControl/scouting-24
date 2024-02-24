bc120644ec46c5b527b7ed49012535428e393d6c
Rafael Lawisch <lawisch.nh@gmail.com>
2024-02-24 16:38:31 -0300

    chore: remove unneeded dependency

--- src/lib/models/LocalStorage.ts
+++ src/lib/models/LocalStorage.ts
@@ -1,6 +1,6 @@
-import { persisted } from 'svelte-persisted-store';
+import { localStorageStore } from '@skeletonlabs/skeleton';
 
-export const settings = persisted('settings', {
+export const settings = localStorageStore('settings', {
 	tournamentLevel: 'Qual',
 	matchNumber: 1,
 	teamStation: 'Red1'
