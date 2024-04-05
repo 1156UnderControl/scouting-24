19f7b9ef33442b636b5e7b627f3ce3a937b434d6
Lucca <81322668+ljb6@users.noreply.github.com>
2024-04-05 10:45:20 -0300

    Update TeleoperatedPeriodStep.svelte

--- src/lib/steps/TeleoperatedPeriodStep.svelte
+++ src/lib/steps/TeleoperatedPeriodStep.svelte
@@ -30,11 +30,11 @@
 		</div>
 		<div class="grid grid-cols-2 justify-items-center gap-2 py-2">
 			<div>
-				<p class="h3 px-2">Passes:</p>
+				<p class="px-2">Passes:</p>
 				<NumberInput bind:value={passes} min={0} max={35} />
 			</div>
 			<div>
-				<p class="h3 px-2">Fouls:</p>
+				<p class="px-2">Fouls:</p>
 				<NumberInput bind:value={fouls} min={0} max={15} />
 			</div>
 		</div>
